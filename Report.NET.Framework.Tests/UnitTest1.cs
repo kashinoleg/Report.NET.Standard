de69d54481da44990ba5fe2efc63732bcdcc1941
Kashin Oleg <kashin.oleg@list.ru>
2019-11-06 15:31:40 +0500

    Fork project from https://github.com/ritchiecarroll/Report.NET

---
+++ Report.NET.Framework.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Root.Reports;
+
+namespace Report.NET.Framework.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ReportBase report = new ReportBase(new PdfFormatter());
+            FontDef fd = new FontDef(report, "Helvetica");
+            FontProp fp = new FontPropMM(fd, 25);
+            Page page = new Page(report);
+
+            page.AddCB_MM(80, new RepString(fp, "Hello World!"));
+
+            report.Save("HelloWorld.pdf");
+
+        }
+    }
+}
